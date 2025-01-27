5498a39a13c8d2ca6b35fd79dadf140d672dadce
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2025-01-27 19:25:19 +0530

    Add files via upload

---
+++ Step3/Medium/Leader.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> Leader(vector<int>arr){
+    int maxi=INT_MIN;
+    vector<int> ans;
+    int n=arr.size();
+    for(int i=n-1;i<arr.size();i--){
+        if(arr[i]>maxi){
+            ans.push_back(arr[i]);
+        }
+
+    }
+    sort(ans.begin(),ans.end());
+    return ans;
+}
