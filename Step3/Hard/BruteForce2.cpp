2ef3808b6381ffe39edb9b6d6a0a48ffb72de471
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2025-01-30 20:28:05 +0530

    Add files via upload

---
+++ Step3/Hard/BruteForce2.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<vector<int>>BF(vector<int>&num,int n){
+    set<vector<int>>st;
+    for(int i=0;i<n;i++){
+        set<int>hashset;
+        for(int j=i+1;j<n;j++){
+            int third=-(num[i]+num[j]);
+            if(hashset.find(third)!=hashset.end()){
+                vector<int>temp={num[i],num[j],third};
+                sort(temp.begin(),temp.end());
+                st.insert(temp);
+            }
+              hashset.insert(num[j]);
+
+
+        }
+        vector<vector<int>>ans(st.begin(),st.end());
+        return ans;
+       
+       
+    }
+}
