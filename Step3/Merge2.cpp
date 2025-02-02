bdbbc9816620c87f638018b4902c625385d8708b
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2025-02-03 00:52:29 +0530

    Add files via upload

---
+++ Step3/Merge2.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int n = intervals.size();
+        if (n == 0) return {}; 
+        
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> ans;
+
+     
+        for (const auto& interval : intervals) {
+            
+            if (ans.empty() || ans.back()[1] < interval[0]) {
+                ans.push_back(interval);
+            } else {
+               
+                ans.back()[1] = max(ans.back()[1], interval[1]);
+            }
+        }
+        return ans;
+    }
+
