86f2366b370efb141134d47898073f273d056142
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2025-02-03 23:13:41 +0530

    Add files via upload

---
+++ Step3/Hard/Missing3.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>Missing(vector<int>a){
+    int n=a.size();
+   
+        long long SN=(n * (n+1)) / 2;
+        long long S2N=(n* (n+1) * (n+2) ) / 6;
+        long long S=0,S2=0;
+         for(int i=0;i<n;i++){
+            S+=a[i];
+            S2=(long long) a[i] * (long long) a[i];
+         }
+        
+            long long val1= S-SN;
+            long long val2=S2=S2N;
+            val2=val2/val1;
+            long long x=(val1+val2)/2;
+            long long y=x-val1;
+            return {(int)x,(int)y};
+
+
+    
+    
+}
