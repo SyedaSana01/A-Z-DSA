f6fd213dbdc381199c305693342c9be3031cd2b0
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2025-02-03 23:24:55 +0530

    Update Missing3.cpp

--- Step3/Hard/Missing3.cpp
+++ Step3/Hard/Missing3.cpp
@@ -1,24 +1,21 @@
-#include<bits/stdc++.h>
-using namespace std;
-vector<int>Missing(vector<int>a){
-    int n=a.size();
+   int n = arr.size();
    
-        long long SN=(n * (n+1)) / 2;
-        long long S2N=(n* (n+1) * (n+2) ) / 6;
-        long long S=0,S2=0;
-         for(int i=0;i<n;i++){
-            S+=a[i];
-            S2=(long long) a[i] * (long long) a[i];
-         }
-        
-            long long val1= S-SN;
-            long long val2=S2=S2N;
-            val2=val2/val1;
-            long long x=(val1+val2)/2;
-            long long y=x-val1;
-            return {(int)x,(int)y};
-
-
+    long long SN = (n * (n + 1)) / 2;
+    long long S2N = (n * (n + 1) * (2 * n + 1)) / 6; // Fixed formula for sum of squares
+    long long S = 0, S2 = 0;
     
+    for (int i = 0; i < n; i++) {
+        S += arr[i];
+        S2 += (long long)arr[i] * (long long)arr[i]; // Fix: Accumulate squares properly
+    }
     
+    long long val1 = S - SN;  // Sum Difference (X - Y)
+    long long val2 = S2 - S2N; // Square Sum Difference (X² - Y²)
+    
+    val2 = val2 / val1;  // (X² - Y²) / (X - Y) gives (X + Y)
+    
+    long long x = (val1 + val2) / 2;  // X (Repeating number)
+    long long y = x - val1;  // Y (Missing number)
+    
+    return {(int)x, (int)y};
 }
