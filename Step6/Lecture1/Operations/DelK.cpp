98ac1422d2bf12e7690e0a8faf19fdba121c3824
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2025-02-22 15:25:55 +0530

    Add files via upload

---
+++ Step6/Lecture1/Operations/DelK.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node (int data1,Node* next1){
+        data=data1;
+        next=next1;
+
+    }
+    Node (int data1){
+        data=data1;
+        next=nullptr;
+    }
+    
+};
+Node* AtoL(vector<int>&arr){
+    Node* head=new Node(arr[0]);
+    Node* mover=head;
+    for(int i=1;i<arr.size();i++){
+        Node*temp=new Node(arr[i]);
+        mover->next=temp;
+        mover=temp;
+    }
+    return head;
+
+}
+Node*print(Node*head){
+    while(head!=NULL){
+        cout<<head->data<<" ";
+        head=head->next;
+    }
+    return head;
+}
+Node* DeleteK(Node*head,int k){
+    if(head==NULL)return head;
+    if(k==1){
+        Node* temp=head;
+        head=head->next;
+        delete(temp);
+        return head;
+
+    }
+    int cnt=0;
+    Node* temp=head;
+    Node*prev=NULL;
+    while(temp!=NULL){
+  
+    cnt++;
+    if(cnt==k){
+    prev->next=prev->next->next;
+    delete(temp);
+    break;
+   }
+
+   prev=temp;
+   temp=temp->next;
+}
+return head;
+}
+
+
+int main(){
+    vector<int>arr={12,5 ,8 ,7};
+    Node* head=AtoL(arr);
+    head=DeleteK(head,2);
+    print(head);
+}
