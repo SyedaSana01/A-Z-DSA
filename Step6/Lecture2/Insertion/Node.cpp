97c2b208d8d6de8181272a4feb8dfb77eab4d594
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2025-02-27 13:58:49 +0530

    Add files via upload

---
+++ Step6/Lecture2/Insertion/Node.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node*next;
+    Node*back;
+
+    Node(int data1,Node* next1,Node* back1){
+        data=data1;
+        next=next1;
+        back=back1;
+    }
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+        back=nullptr;
+    }
+
+};
+Node*AtoDLL(vector<int>&arr){
+    Node*head=new Node(arr[0]);
+    Node*prev=head;
+    for(int i=1;i<arr.size();i++){
+        Node*temp=new Node(arr[i],nullptr,prev);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+}
+void print(Node*head){
+    while(head!=NULL){
+        cout<<head->data<< " ";
+        head=head->next;
+
+    }
+}
+Node*InsertHead(Node*head,int val){
+    Node*newHead=new Node(val,head,nullptr);
+    head->back=newHead;
+
+
+   
+    return newHead;
+}
+void InsertNode(Node*node,int val){
+    Node*prev=node->back;
+    Node*newNode=new Node(val,node,prev);
+    prev->next=newNode;
+    node->back=newNode;
+    
+}
+int main(){
+    vector<int>arr={12,10,3,4};
+    Node*head=AtoDLL(arr);
+    InsertNode(head->next->next,1);
+    print(head);
+}
