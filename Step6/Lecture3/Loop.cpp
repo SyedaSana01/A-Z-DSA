a9205ed0411c697544d74e810dfb3f7822ce1c49
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2025-02-27 20:01:15 +0530

    Add files via upload

---
+++ Step6/Lecture3/Loop.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node*next;
+    Node*back;
+
+    Node(int data1,Node* next1,Node* back1){
+        data=data1;
+        next=next1;
+        back=back1;
+    }
+    Node(int data1){
+        data=data1;
+        next=nullptr;
+        back=nullptr;
+    }
+
+};
+Node*AtoDLL(vector<int>&arr){
+    Node*head=new Node(arr[0]);
+    Node*prev=head;
+    for(int i=1;i<arr.size();i++){
+        Node*temp=new Node(arr[i],nullptr,prev);
+        prev->next=temp;
+        prev=temp;
+    }
+    return head;
+
+}
+void print(Node* head) {
+    Node* temp = head;
+    while (temp != nullptr) {
+        cout << temp->data << " ";
+        temp = temp->next;  // **Move to the next node**
+    }
+    cout << endl;
+}
+bool Cycle(Node*head){
+    Node*slow=head;
+    Node*fast=head;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            return true;
+        }
+       
+    }
+    return false;
+}
+
+int main(){
+    vector<int> arr = {12, 14, 15, 6};
+    Node* head = AtoDLL(arr);
+     cout<<Cycle(head);
+    return 0;
+}
+   
